3c87f1221fa5fdeab0e3ac11ce6df0fa6caa906b
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-22 21:53:47 +0530

    ui complete

--- app/account/page.js
+++ app/account/page.js
@@ -4,5 +4,7 @@ export const metadata = {
 
 
 export default function Page() {
-  return <h1>Your account</h1>;
+  return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+  welcome, sameer
+</h2>;
 }
