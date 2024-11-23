6674c5d60de04027fd4fa6a959150b5ce2063164
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-23 16:17:58 +0530

    supabase setup

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,15 @@
 import CabinCard from "@/components/CabinCard";
+import { getCabins } from "@/lib/data-service";
 
 export const metadata = {
-  title : 'Cabin'
+  title : 'Cabins'
 }
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins()
+
+  console.log(cabins)
 
   return (
     <div>
