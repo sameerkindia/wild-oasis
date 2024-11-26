7ce25aaafb5337e87591fbd5858a8f566895320e
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-26 09:54:48 +0530

    not-found and error

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/lib/data-service";
+import { getCabin, getCabins } from "@/lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -19,6 +19,14 @@ export async function generateMetadata({params}){
   const {name} = await getCabin(params.cabinId);
 
   return {title : `Cabin ${name}`}
+}
+
+export async function generateStaticParams(){
+  const cabins = await getCabins()
+
+  const ids = cabins.map((cabin)=>({cabinId:String(cabin.id)}))
+
+  return ids
 }
 
 export default async function Page({params}) {
