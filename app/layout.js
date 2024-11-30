6bcf01e183c6c7069f767296da3990e6e161df0a
sameerkindia <sameerkhanaa5@gmail.com>
2024-11-30 13:15:20 +0530

    context api and api end points

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,34 @@
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
-import {Josefin_Sans} from 'next/font/google'
+import { Josefin_Sans } from "next/font/google";
 
-const josefin = Josefin_Sans({subsets:["latin"], display:'swap'})
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
-import "./../styles/globals.css"
+import "./../styles/globals.css";
 import Header from "@/components/Header";
+import { ReservationProvider } from "@/components/ReservationContext";
 
 export const metadata = {
   title: {
-    template:"%s | The Wild Oasis",
-    default:"Welcome | The Wild Oasis"
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites"
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} antialiased bg-primary-950 min-h-screen text-primary-100 flex flex-col relative`}>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 min-h-screen text-primary-100 flex flex-col relative`}
+      >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-        <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
