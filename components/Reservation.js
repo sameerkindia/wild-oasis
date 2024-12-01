1b4656f427de6ed7466ae3cc5b6c0daffde36524
sameerkindia <sameerkhanaa5@gmail.com>
2024-12-01 12:01:51 +0530

    auth js

--- components/Reservation.js
+++ components/Reservation.js
@@ -1,6 +1,8 @@
 import { getBookedDatesByCabinId, getSettings } from "@/lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import LoginMessage from "./LoginMessage";
+import { auth } from "@/lib/auth";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
@@ -8,10 +10,20 @@ async function Reservation({ cabin }) {
     getBookedDatesByCabinId(cabin.id),
   ]);
 
+  const session = await auth();
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-      <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
