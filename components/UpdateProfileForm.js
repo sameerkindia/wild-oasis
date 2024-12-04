dcb5a4fe2071d23a35a5a63b5e08c92bf6bd316c
sameerkindia <sameerkhanaa5@gmail.com>
2024-12-04 10:24:56 +0530

    server action for updating user profile

--- components/UpdateProfileForm.js
+++ components/UpdateProfileForm.js
@@ -1,71 +1,73 @@
-'use client'
+"use client";
 
+import { updateGuest } from "@/lib/actions";
 
-function UpdateProfileForm({guest, children}) {
-
+function UpdateProfileForm({ guest, children }) {
   // const countryFlag = "pt.jpg";
   // const nationality = "portugal";
-  console.log(guest)
+  console.log(guest);
 
-  const {fullName, email, natinoality, nationalId, countryFlag} = guest;
-
+  const { fullName, email, natinoality, nationalID, countryFlag } = guest;
 
   return (
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
-        <div className="space-y-2">
-          <label>Full name</label>
-          <input
-            disabled
-            defaultValue={fullName}
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+    <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <div className="space-y-2">
+        <label>Full name</label>
+        <input
+          disabled
+          defaultValue={fullName}
+          name="fullName"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label>Email address</label>
+        <input
+          disabled
+          defaultValue={email}
+          name="email"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <label htmlFor="nationality">Where are you from?</label>
+          <img
+            src={countryFlag}
+            name="countryFlag"
+            alt="Country flag"
+            className="h-5 rounded-sm"
           />
         </div>
 
-        <div className="space-y-2">
-          <label>Email address</label>
-          <input
-            disabled
-            defaultValue={email}
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <div className="flex items-center justify-between">
-            <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryFlag}
-              alt="Country flag"
-              className="h-5 rounded-sm"
-            />
-          </div>
-
-          {/* <SelectCountry
+        {/* <SelectCountry
             name="nationality"
             id="nationality"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             defaultCountry={nationality}
           /> */}
 
-          {children}
-        </div>
+        {children}
+      </div>
 
-        <div className="space-y-2">
-          <label htmlFor="nationalID">National ID number</label>
-          <input
-          defaultValue={nationalId}
-            name="nationalID"
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          />
-        </div>
+      <div className="space-y-2">
+        <label htmlFor="nationalID">National ID number</label>
+        <input
+          defaultValue={nationalID}
+          name="nationalID"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+        />
+      </div>
 
-        <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
-        </div>
-      </form>
-  )
+      <div className="flex justify-end items-center gap-6">
+        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          Update profile
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default UpdateProfileForm
+export default UpdateProfileForm;
