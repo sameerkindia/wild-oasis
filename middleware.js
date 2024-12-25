adc18c333275164c7ff0f1a9a1f539b7d4d0d8f7
sameerkindia <sameerkhanaa5@gmail.com>
2024-12-25 11:41:09 +0530

    build error

--- middleware.js
+++ middleware.js
@@ -2,13 +2,9 @@
 
 import { auth } from "./lib/auth"
 
-// export function middleware(request){
-//   return NextResponse.redirect(new URL("/about", request.url))
-// }
-
-
 export const middleware = auth
 
 export const config = {
+  runtime: 'nodejs',
   matcher: ["/account"]
 }
