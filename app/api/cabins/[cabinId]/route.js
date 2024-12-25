4e7b959425629edb52a549c716cdf88158020ea5
sameerkindia <sameerkhanaa5@gmail.com>
2024-12-25 11:04:11 +0530

    mongoDB added

--- app/api/cabins/[cabinId]/route.js
+++ app/api/cabins/[cabinId]/route.js
@@ -1,11 +1,12 @@
-import { getBookedDatesByCabinId, getCabin } from "@/lib/data-service";
+import { getBookedDatesByCabinIdServer, getOneCabin } from "@/lib/actions";
+// import { getBookedDatesByCabinId, getCabin } from "@/lib/data-service";
 
 export async function GET(request , {params}) {
 
   const {cabinId} = params;
 
   try{
-    const [cabin, bookedDates] = await Promise.all([getCabin(cabinId), getBookedDatesByCabinId(cabinId)])
+    const [cabin, bookedDates] = await Promise.all([getOneCabin(cabinId), getBookedDatesByCabinIdServer(cabinId)])
 
     return Response.json({cabin,bookedDates})
   } catch {
