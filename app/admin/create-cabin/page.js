3ab339a577a1f012b0738c69fa990ca0233d542a
sameerkindia <sameerkhanaa5@gmail.com>
2025-01-04 12:55:18 +0530

    admin page setup

---
+++ app/admin/create-cabin/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Create Cabin page</div>
+  )
+}
+
+export default page
---
+++ app/admin/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome Admin</h1>
+
+      <section>
+        
+      </section>
+    </div>
+  )
+}
+
+export default page
