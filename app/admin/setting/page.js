48efaf9c2244efcc31d8fc0b10574980d9017952
sameerkindia <sameerkhanaa5@gmail.com>
2025-01-10 09:36:32 +0530

    setting edit page

--- app/admin/setting/page.js
+++ app/admin/setting/page.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import { getSettingsServer } from "@/lib/actions";
+import Link from "next/link";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const {
+    minBookingLength,
+    maxBookingLength,
+    maxGuestPerBooking,
+    breakfastPrice,
+  } = await getSettingsServer();
+
   return (
-    <h3>Setting page</h3>
+    <section>
+      <div className="flex items-center justify-between gap-4">
+      <h3 className="font-semibold text-2xl text-accent-400 mb-7">
+        Setting For Booking
+      </h3>
+      <Link href='/admin/setting/edit'>Edit</Link>
+      </div>
 
-    // <section></section
-  )
-}
+      <div className="grid grid-cols-2 gap-4">
+        <p className="flex gap-4 text-lg text-primary-200">
+          Breakfast Price <span className="font-bold">{breakfastPrice}</span>
+        </p>
+        <p className="flex gap-4 text-lg text-primary-200">
+        Min Booking Length <span className="font-bold">{minBookingLength}</span>
+        </p>
+        <p className="flex gap-4 text-lg text-primary-200">
+        Max Booking Length <span className="font-bold">{maxBookingLength}</span>
+        </p>
+        <p className="flex gap-4 text-lg text-primary-200">
+        Max Guest Per Booking <span className="font-bold">{maxGuestPerBooking}</span>
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
